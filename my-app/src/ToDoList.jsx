5ced7d13ef0d082d600632e121c26c6413ced378
amb2007 <agustinmancinibobadilla@gmail.com>
2024-07-24 11:26:04 -0300

    act10

--- my-app/src/ToDoList.jsx
+++ my-app/src/ToDoList.jsx
@@ -1,33 +1,63 @@
-import { useState } from 'react';
+import { act, useState } from 'react';
 
 export default function Titulo() {
-    const [Tasks, satTasks] = useState([]);
-    const [Task, setTask] = useState('');
+    const [Tasks, setTasks] = useState([])
+
+    const [Task, setTask] = useState({
+        act: '',
+        cat: ''
+    });
+
     const handleChange = (event) => {
-        setTask (event.target.value)
-        };
+        const { name, value } = event.target;
+        setTask({
+            ...Task,
+            [name]: value
+            });
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        window.alert('Formulario enviado');
-            };
-    return(<div>
-        
+        setTask({
+            act: '',
+            cat: ''
+        })
+    };
+    const RemoveTask = (index) =>{
+        const filtrado = Tasks.filter((tarea, i) =>
+            i !== index
+        )
+        setTasks(filtrado)
+    }
+    return (<div>
+
         <div className="App">
             <header className="App-header">
                 <h1>Lista De Cosas Para Hacer</h1>
             </header>
         </div>
-        <form onSubmit={handleSubmit}/>
-        <div>
-            <label htmlFor="nombre">Nombre:</label>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="Tarea">Tarea:</label>
             <input
-            type="text"
-            id="nombre" 
-            name="nombre"
-            value={Task}
-            onChange={handleChange}
+                type="text"
+                id="Tarea"
+                name="Tarea"
+                value={Task.act}
+                onChange={handleChange}
             />
-        </div>
+            <label htmlFor="Categoria">Tarea:</label>
+            <input
+                type="text"
+                id="Categoria"
+                name="Categoria"
+                value={Task.cat}
+                onChange={handleChange}
+            />
+            <button type="submit">Enviar</button>
+        </form>
+        <ul>
+            {Tasks.map((element,i) => <li id={i}>{element}  <button onClick={()=>RemoveTask(i)}  >eliminar</button></li>)}
+        </ul>
     </div>
     )
 }
